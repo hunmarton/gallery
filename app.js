fe813e744b0e8c559c602c7b4f780800f57aed83
Marton <hunmarton@hotmail.com>
2020-07-23 21:52:07 +0200

    left and right arrows working

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const images = document.querySelectorAll('#img');
 const bigImage = document.querySelector('.big-image');
 const bigImg = document.querySelector('.big-img');
 const close = document.querySelector('.close');
-
-const imagesArr = Array.from(images);
-console.log(imagesArr);
+const left = document.querySelector('.fa-arrow-left');
+const right = document.querySelector('.fa-arrow-right');
 
 images.forEach(image => {
     image.addEventListener('click', () => {
@@ -12,6 +11,25 @@ images.forEach(image => {
         bigImage.style.display = 'flex';
         bigImage.classList.add('visible');
         bigImg.style.backgroundImage = data;
+        let currentImg = image.dataset.num;
+        right.addEventListener('click', () => {
+            if (currentImg < images.length - 1) {
+                currentImg++;
+                bigImg.style.backgroundImage = images[currentImg].dataset.url;
+            } else {
+                currentImg = 0;
+                bigImg.style.backgroundImage = images[currentImg].dataset.url;
+            }
+        });
+        left.addEventListener('click', () => {
+            if (currentImg > 0 && currentImg < images.length) {
+                currentImg--;
+                bigImg.style.backgroundImage = images[currentImg].dataset.url;
+            } else {
+                currentImg = images.length - 1;
+                bigImg.style.backgroundImage = images[currentImg].dataset.url;
+            }
+        });
     });
 });
 
